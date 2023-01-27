300459fb53ad51cfb87605512fc6a18eeeba398c
HelenaSF95 <helenafuglsbjerg@live.dk>
2023-01-27 14:29:28 +0100

    Added SheCodes weather API

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-//alert(`Hi`);
+function getWeather(response) {
+  console.log(response);
+}
+
+let apiKey = `abodftdf7899f82673d6451a0b0db4af`;
+let city = `Paris`;
+let units = `metric`;
+let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+axios.get(url).then(getWeather);
